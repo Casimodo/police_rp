1acbdb048f0f68e7032bb2eabea6fa94f90dd328
Fredo AlienswareFixe <lorsignol@gmail.com>
2023-07-18 19:57:32 +0200

    ajout code penal

---
+++ routers_http/cdepenal_http.js
@@ -0,0 +1,55 @@
+module.exports = function (config, app, logger, ensureAuthenticated, passport) {
+
+    let self = this;
+    const path = require('path');
+    const uid = require('uid');
+    self.database = require('../class/datasdb.class.js');
+    self.db = new self.database(config, logger);
+    self.outilsClass = require('../class/utils.class.js');
+    self.outils = new self.outilsClass();
+
+    try {
+
+        /** ***********************************************
+         *
+         *********************************************** */
+        app.get("/cdepenal", ensureAuthenticated, function (req, res) {
+
+            const coplevel = parseInt(req.user.player.grade);
+
+            if (coplevel > 0) {
+
+                db.connect(true).then(conn => {
+                    db.code_penal(conn).then(datas => {
+
+                        res.render("pages/code_penal/index.ejs", {
+                            PARAMS: req.PARAMS,
+                            I18N: req.I18N,
+                            page_name: 'cdepenal',
+                            user: req.user,
+                            datasCdePenal: datas
+                        });
+
+                        conn.end();
+
+                    }, err => {
+                        logger.error(err);
+                    });
+                }, err => {
+                    logger.error(err);
+                });
+
+            } else {
+                res.redirect('/logout');
+            }
+
+        });
+
+
+
+    } catch (err) {
+        console.error("ERROR purge");
+    }
+
+
+}
